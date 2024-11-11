f4b3ff7b84e316d818e77168ba26959165fb0754
charlesfabri <charles_fabri@hotmail.com>
2024-11-11 14:52:42 -0300

    Add recursos ao aplicativo.

--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,6 +13,14 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Oswald-Bold.ttf", "OswaldBold");
+                    fonts.AddFont("Oswald-Light.ttf", "OswaldLight");
+                    fonts.AddFont("Oswald-ExtraLight", "OswaldExtraLight");
+                    fonts.AddFont("Oswald-Medium.ttf", "OswaldMedium");
+                    fonts.AddFont("Oswald-Regular.ttf", "OswaldRegular");
+                    fonts.AddFont("Oswald-SemiBold.ttf", "OswaldSemiBold");
+
+
                 });
 
 #if DEBUG
